be19524581e9cd3bf8f0af2476abd769cf9202e9
Yohann Boniface <edhyjox@gmail.com>
2021-09-03 23:59:55 +0200

    ✨ Exercise 14

--- first_year/tp_4/exercice_14.py
+++ first_year/tp_4/exercice_14.py
@@ -1,6 +1,21 @@
-def seven_multiple(x: int) -> None:
-    print(*(f"{7 * i}%s" % ('*' * (not 7 * i % 3)) for i in range(1, x)))
+def time(s: int) -> str:
+    return (
+        "{0}s -> {3}h, {2}m {1}s".format(
+            s, *((s % 60, s := s // 60)[0] for _ in range(2)), s
+        )
+    )
+
+
+def seven_multiple(x: int) -> str:
+    return ' '.join(
+        f"{7 * i}%s" % ('*' * (not 7 * i % 3)) for i in range(1, x)
+    )
+
+
+def main() -> None:
+    print(seven_multiple(21))
+    print(time(int(input("s: "))))
 
 
 if __name__ == '__main__':
-    seven_multiple(21)
+    main()
