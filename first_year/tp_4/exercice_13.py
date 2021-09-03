255b8954aa0a58036da50a5210ca628e69fbfdd4
Yohann Boniface <edhyjox@gmail.com>
2021-09-03 23:53:37 +0200

    ✨ Exercise 13

---
+++ first_year/tp_4/exercice_13.py
@@ -0,0 +1,15 @@
+def factorial(x):
+    r = 1
+    for i in range(1, x + 1):
+        r *= i
+
+    return r
+
+
+def main():
+    nombre = int(input("Entrez votre nombre: "))
+    print(f"La factorielle de {nombre} est: {factorial(r)}")
+
+
+if __name__ == '__main__':
+    main()
